7e647e807182c991a22a8d84ceaee95383f24685
JBDeslandes <jbdeslandes@hotmail.fr>
2018-11-28 17:19:43 +0100

    Initial Commit

---
+++ Open Quizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Open Quizz
+//
+//  Created by Jean-Baptiste Deslandes on 28/11/2018.
+//  Copyright © 2018 Jean-Baptiste Deslandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
